99d49d6ecc49d94493c24616c33df733293b015e
HEEXIONG <qqppoo94@naver.com>
2022-09-30 16:04:46 +0900

    부트 crud

---
+++ src/main/java/com/springboot/board/SpringBootOracleDbCrudApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOracleDbCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOracleDbCrudApplication.class, args);
+		System.out.println("안녕 장나라");
+	}
+
+}
